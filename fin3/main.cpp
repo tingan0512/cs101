fb31bf6f98e3b36e29078d1a82b79e2f7ce4cd70
江庭安 <90617728+tingan0512@users.noreply.github.com>
2022-06-23 16:45:50 +0800

    Update main.cpp

--- fin3/main.cpp
+++ fin3/main.cpp
@@ -1,44 +1,36 @@
 #include <iostream>
 using namespace std;
 
-class myArray{
+class myArray {
     public:
         int n;
         int* array;
         int size;
-        
-        myArray(int size){
-            this->size = size;
+        myArray(int size) {
+            this -> size = size;
         }
-        
-        void recInput(int array[], int n){
-            this->array = &array[0];
-            this->n = n; 
+        void recInput(int array[], int n) {
+            this -> array = &array[0];
+            this -> n = n; 
         }
-        
-        int recMax(int n){
-            if(n == 1){
+        int recMax(int n) {
+            if(n == 1) {
                 return array[0];
             }
-            
             return max(recMax(n-1), array[n-1]);
         }
-        
-        int recSum(int n){
-            if(n == 1){
+        int recSum(int n) {
+            if(n == 1) {
                 return array[0];
             }
-            
             return recSum(n-1) + array[n-1];
         }
-        
-        float recAve(int n){
-            return recSum(n)/(float)n;
+        float recAve(int n) {
+            return recSum(n) / (float)n;
         }
-    
 };
 
-int main(){
+int main() {
     int size = 5;
     int array[] = {500,1,2,3,4};
     myArray A(size);
@@ -46,6 +38,5 @@ int main(){
     cout << "The max is: " << A.recMax(size) << endl; 
     cout << "The sum is: " << A.recSum(size) << endl; 
     cout << "The avr is: " << A.recAve(size) << endl; 
-    
     return 0;
 }
